5392c7945b8d047e8ca2ec18d447105010cb4f6b
Sean OMeara <sean@chef.io>
2015-02-13 18:32:24 -0500

    savegame: pear working on centos-5 / php 5.3

--- libraries/provider_phpruntime_package.rb
+++ libraries/provider_phpruntime_package.rb
@@ -7,20 +7,52 @@ class Chef
 
         action :install do
           configure_package_repositories if new_resource.manage_package_repos
-          
-          if el5_php53?
-          else
-            package 'php-pear' do
-              action :install
-            end
-          end
-          
+
+          cache_path = Chef::Config[:file_cache_path]
+
           # iterate over packages..
           # either supplied as resource parameters, or default values
           parsed_runtime_packages.each do |pkg|
             package "#{new_resource.name} :install #{pkg[:pkg_name]}" do
               package_name pkg[:pkg_name]
               version pkg[:pkg_version]
+              action :install
+            end
+          end
+
+          if el5_php53?
+            package "expect" do
+              action :install
+            end
+
+            cookbook_file "#{new_resource.name} :install #{cache_path}/go-pear.phar" do
+              path "#{cache_path}/go-pear.phar"
+              source 'go-pear.phar'
+              cookbook 'php'
+              owner 'root'
+              group 'root'
+              mode '0755'
+              action :create
+            end
+
+            template "#{new_resource.name} :install #{cache_path}/anykey.exp" do
+              path "#{cache_path}/anykey.exp"
+              source 'anykey.exp.erb'
+              cookbook 'php'
+              owner 'root'
+              group 'root'
+              mode '0755'
+              variables(cache_path: cache_path)
+              action :create
+            end
+
+            execute "#{new_resource.name} :install /usr/bin/pear" do
+              command "#{cache_path}/anykey.exp"
+              creates '/usr/bin/pear'
+              action :run
+            end
+          else
+            package 'php-pear' do
               action :install
             end
           end
